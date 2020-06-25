248392acee0ad5d7cd9591827bd33d64b6bf81b4
Istern22 <alistern22@gmail.com>
2020-06-25 21:29:59 +1000

    Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -13,57 +13,23 @@ import java.util.Locale;
 
 public class SqlRuParse {
 
-    public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            Element date = td.parent().child(5);
-            System.out.println(date.text() + "\n");
-        }
-    }
+    private static int hrefIndex = 0;
+    private static int dateIndex = 5;
+    private static String attrKey = "href";
 
-    public static String toDate(String dateString) {
-        LocalDateTime formatDate;
-        if (dateString.contains("сегодня")) {
-            formatDate = LocalDateTime.now();
-        } else if (dateString.contains("вчера")) {
-            LocalDateTime now = LocalDateTime.now();
-            formatDate = now.minusDays(1);
-        } else {
-            String[] items = dateString.split(" ");
-            switch (items[1]) {
-                case "янв" : items[1] = "01";
-                    break;
-                case "фев" : items[1] = "02";
-                    break;
-                case "мар" : items[1] = "03";
-                    break;
-                case "апр" : items[1] = "04";
-                    break;
-                case "май" : items[1] = "05";
-                    break;
-                case "июн" : items[1] = "06";
-                    break;
-                case "июл" : items[1] = "07";
-                    break;
-                case "авг" : items[1] = "08";
-                    break;
-                case "сен" : items[1] = "09";
-                    break;
-                case "окт" : items[1] = "10";
-                    break;
-                case "ноя" : items[1] = "11";
-                    break;
-                case "дек" : items[1] = "12";
-                    break;
-                default : break;
+    public static void main(String[] args) throws IOException {
+        int page = 0;
+        while (page < 6) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + page).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(hrefIndex);
+                System.out.println(href.attr(attrKey));
+                System.out.println(href.text());
+                Element date = td.parent().child(dateIndex);
+                System.out.println(date.text() + System.lineSeparator());
             }
-            dateString = String.join(" ", items);
-            formatDate = LocalDateTime.parse(dateString, DateTimeFormatter.ofPattern("d MM yy, HH:mm"));
+            page++;
         }
-        return formatDate.format(DateTimeFormatter.ofPattern("dd MMMM yyyy, HH:mm", new Locale("ru")));
     }
 }
